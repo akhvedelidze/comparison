828cd48686e4ae6ec8d4c5813b7af8c6596efffa
akhvedelidze <akhvedelidze@gmail.com>
2019-09-26 14:14:44 -0400

    added eldest function

--- script.js
+++ script.js
@@ -2,41 +2,48 @@
 
 function whoIsOlder(person1, person2) {
   if (person1.age > person2.age) {
-   
-    console.log(person1.name + " is " + (person1.age-person2.age) + " year(s) older then " + person2.name);
+    console.log(person1.name + " is " + (person1.age - person2.age) + " year(s) older then " + person2.name);
+  } else if (person1.age < person2.age) {
+    console.log(person2.name + " is " + (person2.age - person1.age) + " year(s) older then " + person1.name);
+  }
+  else { console.log(person2.name + " and " + person1.name + " have same age"); }
+}
 
-  } else if  (person1.age < person2.age) {
-   
-    console.log(person2.name + " is " + (person2.age-person1.age) + " year(s) older then " + person1.name);
+function whoIsOldest(person1, person2, person3) {
+
+  if (person1.age > person2.age && person1.age > person3.age) {
+    console.log(person1.name + " is eldest");
+  }
+  if (person2.age > person1.age && person2.age > person3.age) {
+    console.log(person2.name + " is eldest");
+  }
+  if (person3.age > person1.age && person3.age > person2.age) {
+    console.log(person3.name + " is eldest");
   }
 
-  else 
-  {console.log (person2.name + " and " + person1.name + " have same age");   }
-
-  
-};
+  else { console.log ("No one is eldest in this group"); } }
 
 
-var me = {
-  name: 'Akaki',
-  age: 38
-};
+  var me = {
+    name: 'Akaki',
+    age: 38
+  };
 
 
-var shota = {
-  name: 'Shota',
-  age: 41
-};
+  var shota = {
+    name: 'Shota',
+    age: 41
+  };
 
 
-var drini = {
-  name: 'Drini',
-  age: 41
-};
+  var drini = {
+    name: 'Drini',
+    age: 41
+  };
 
 
-
-whoIsOlder(me, shota);
-whoIsOlder(me, drini);
-whoIsOlder(drini, shota);
-
+  whoIsOlder(me, shota);
+  whoIsOlder(me, drini);
+  whoIsOlder(drini, shota);
+  whoIsOldest(me, shota, drini);
+ 
